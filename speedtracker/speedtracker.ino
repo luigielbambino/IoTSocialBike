3a0dc65970bcc3518268668481cd1044fc7f4f48
luigielbambino <luis_al82@hotmail.com>
2019-04-21 22:55:09 +0200

    Update speed tracker
    Reading magnetic switch and boolean variables

--- speedtracker/speedtracker.ino
+++ speedtracker/speedtracker.ino
@@ -1,32 +1,28 @@
 // Reed switch digital input
 int digitalPin = 32;
-int digital_value = 0;
-String init_message = "ESP32 Analog and Digital IN readings!";
-String ping_message = "Ping";
-bool switch_value = false;
+int sensor_value;
+String init_message = "Starting Speed-sensor readings";
+bool switch_value;
+float t_time;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(digitalPin, INPUT); 
   Serial.println(init_message);
-  delay(100);
+  pinMode(digitalPin, INPUT);
+  switch_value = false;
+  t_time = 0;
+  sensor_value = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digital_value = digitalRead(digitalPin);
-  Serial.println(ping_message);
-  Serial.println(digital_value);
-  if (digital_value == HIGH) 
-  {
-    Serial.println("High Value");
-    Serial.println(digital_value);
+  sensor_value = digitalRead(digitalPin);
+  if (sensor_value == HIGH){
+    if(switch_value == false){
+      switch_value = true;
+      Serial.println(sensor_value);
+    }
+  } else{
+    switch_value = false;
+    Serial.println(sensor_value);
   }
-  else
-  {
-    Serial.println("Low Value");
-    Serial.println(digital_value);
-  }
-  delay(80);
 }
